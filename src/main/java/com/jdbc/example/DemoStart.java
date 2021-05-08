ef7b37c438839f127769a7831ee70e8bb130f5c6
yuyenews <631941527@qq.com>
2021-05-08 14:14:13 +0800

    升级依赖到1.1.2

--- src/main/java/com/jdbc/example/DemoStart.java
+++ src/main/java/com/jdbc/example/DemoStart.java
@@ -4,9 +4,9 @@ import com.alibaba.druid.pool.DruidDataSource;
 import com.jdbc.example.model.DemoDTO;
 import com.magician.jdbc.MagicianJDBC;
 import com.magician.jdbc.core.constant.enums.TractionLevel;
-import com.magician.jdbc.core.traction.TractionManager;
-import com.magician.jdbc.core.util.JSONUtil;
+import com.magician.jdbc.core.transaction.TransactionManager;
 import com.magician.jdbc.helper.templete.JdbcTemplate;
+import com.magician.jdbc.sqlbuild.SqlBuilder;
 import com.mysql.cj.jdbc.Driver;
 
 import java.util.Date;
@@ -17,11 +17,11 @@ public class DemoStart {
 
     public static void main(String[] args) throws Exception {
 
-        TractionManager.beginTraction(TractionLevel.READ_COMMITTED);
-
-        TractionManager.commit();
-
-        TractionManager.rollback();
+//        TransactionManager.beginTraction(TractionLevel.READ_COMMITTED);
+//
+//        TransactionManager.commit();
+//
+//        TransactionManager.rollback();
 
         /*
          * 首先需要创建数据源
@@ -47,23 +47,33 @@ public class DemoStart {
 
         /* ************** 操作数据库，这些代码 在实战中需要写到对应的DAO里面 ************ */
         // 从test表查询主键=102的数据
-        DemoDTO demoDTO = JdbcTemplate.create().getOneByPrimaryKey("test","id", 102, DemoDTO.class);
+        String sql = SqlBuilder.select("test").byPrimaryKey("id").builder();
+        DemoDTO param = new DemoDTO();
+        param.setId(102);
+        DemoDTO demoDTO = JdbcTemplate.create().selectOne(sql, param, DemoDTO.class);
 
-        // 删除主键=104的数据
-        JdbcTemplate.create().deleteByPrimaryKey("test","id", 104);
+        // 删除主键=107的数据
+        String sql2 = SqlBuilder.delete("test").byPrimaryKey("id").builder();
+        DemoDTO param2 = new DemoDTO();
+        param2.setId(107);
+        JdbcTemplate.create().update(sql2, param2);
 
         // 将DEO保存到数据库
         DemoDTO demo = new DemoDTO();
         demo.setCreateTime(new Date());
         demo.setName("testName");
-        JdbcTemplate.create().insert("test", demo);
 
-        // 修改主键=100的数据的 name为testName，createTime为当前时间
+        String sql3 = SqlBuilder.insert("test").column(DemoDTO.class).builder();
+        JdbcTemplate.create().update(sql3, demo);
+
+        // 修改主键=105的数据的 name为testName，createTime为当前时间
         DemoDTO demo2 = new DemoDTO();
         demo2.setCreateTime(new Date());
         demo2.setName("testName");
-        demo2.setId(103);
-        JdbcTemplate.create().updateByPrimaryKey("test","id", demo2);
+        demo2.setId(105);
+
+        String sql5 = SqlBuilder.update("test").column(DemoDTO.class).where("id = #{id}").builder();
+        JdbcTemplate.create().update(sql5, demo2);
 
         // 查询name=testName的数据
         DemoDTO demo3 = new DemoDTO();
